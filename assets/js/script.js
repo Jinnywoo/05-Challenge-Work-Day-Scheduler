b17b497d71b058b0c64cd3cf8f25549aa9f24e1c
Ares <jinwoo2639@gmail.com>
2022-11-11 04:20:21 -0800

    added the HTML rendering for boxes and added distinctions for am pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,38 @@ var timeEntries =document.getElementById("timeEntries");
 
 var currentTime = moment().format('LLL');
 
+var blockArray =[];
+for(var i=9; i < 18; i++){
+    
+    var timeStamp = `hour${i}`;
+    var saved = localStorage.getItem(timeStamp);
+    if (data === null){
+        data = '';
+    }
+
+    var time = '';
+    var midday ='';
+
+    if(i <div 12){
+        midday = 'AM';
+    }else{
+        midday = 'PM';
+    }
+
+    var outputHTML = document.createElement('div');
+    outputHTML.innerHTML =`
+    <div class ="row">
+        <div class = "hour">
+        ${i}${midday}
+        </div>
+        <textarea id = "textarea${i}" class="${time}">${data}</textarea>
+        <button class = "saveBtn" data-hour="${i}">
+            <span class="materia-symbols-outlined">
+                save
+            </span>
+        </button>
+        </div>
+    </div>
+    `;
+    timeEntries.appendChild(outputHTML);
+};
