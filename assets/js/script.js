feb61c1413e85a9e4807486a96e62426607ec835
Ares <jinwoo2639@gmail.com>
2022-11-11 00:01:32 -0800

    nevermind lets use an array to render the blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,5 @@
-var today = document.getElementById("currentDay");
+var currentDay = document.getElementById("currentDay");
+var timeEntries =document.getElementById("timeEntries");
 
-var sch_9am = document.getElementById("scheduleBox_9am");
-var sch_10am = document.getElementById("scheduleBox_10am");
-var sch_11am = document.getElementById("scheduleBox_11am");
-var sch_12pm = document.getElementById("scheduleBox_12pm");
-var sch_1pm = document.getElementById("scheduleBox_1pm");
-var sch_2pm = document.getElementById("scheduleBox_2pm");
-var sch_3pm = document.getElementById("scheduleBox_3pm");
-var sch_4pm = document.getElementById("scheduleBox_4pm");
-var sch_5pm = document.getElementById("scheduleBox_5pm");
-
-
+var currentTime = moment().format('LLL');
 
